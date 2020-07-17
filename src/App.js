e809557b611d893c6a53a6432c519e0dea6fc8b8
Erick Saavedra <esvdrah@gmail.com>
2020-07-17 12:53:13 -0500

    add @emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  border: 2px solid black;
+`;
 
 function App() {
-  return <h1>Breackingbad</h1>;
+  const requestAPI = () => {
+    console.log('request...');
+  };
+
+  return (
+    <Container>
+      <Button onClick={requestAPI /* () => requestAPI() or requestAPI() */}>
+        Get Quote
+      </Button>
+    </Container>
+  );
 }
 
 export default App;
